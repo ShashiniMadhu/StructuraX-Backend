eb74aaee18f6999d892ae61863d33cb69ec72cc5
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-07-15 09:26:53 +0530

    rename file

--- src/main/java/com/structurax/root/structurax/root/dto/SiteVisitLogDTO.java
+++ src/main/java/com/structurax/root/structurax/root/dto/SiteVisitLogDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 public class SiteVisitLogDTO {
     private Integer id;
 
+
     @JsonProperty("project_id")
     private Integer projectId;
     private LocalDate date;
