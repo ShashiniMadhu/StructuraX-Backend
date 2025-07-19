38fa3bc96e9ee9b8983eeb277972efd615edf3d1
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-07-19 20:49:06 +0530

    update project manager backend part

--- src/main/java/com/structurax/root/structurax/root/service/ProjectManagerService.java
+++ src/main/java/com/structurax/root/structurax/root/service/ProjectManagerService.java
@@ -1,10 +1,15 @@
 package com.structurax.root.structurax.root.service;
 
 import com.structurax.root.structurax.root.dto.SiteVisitLogDTO;
+import com.structurax.root.structurax.root.dto.VisitRequestDTO;
+
 import java.util.List;
 
 public interface ProjectManagerService {
     SiteVisitLogDTO createVisitLog(SiteVisitLogDTO dto);
     List<SiteVisitLogDTO> getAllVisitLogs();
     SiteVisitLogDTO getVisitLogById(Integer id);
+    boolean updateVisitLog(SiteVisitLogDTO dto);
+    List<VisitRequestDTO> getAllVisitRequests();
+    boolean updateVisitRequest(VisitRequestDTO dto);
 }
