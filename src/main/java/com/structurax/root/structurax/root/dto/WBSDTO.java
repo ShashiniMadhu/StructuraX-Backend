1104609a8fe383ee7b2bfd03a9c46e0d9e4c7abc
Malith39 <sandaruwanmalith39@gmail.com>
2025-08-14 01:51:53 +0530

    Add RequestSiteResourcesDTO and implement methods for retrieving and updating site resource requests in QSService and QSServiceImpl

--- src/main/java/com/structurax/root/structurax/root/dto/WBSDTO.java
+++ src/main/java/com/structurax/root/structurax/root/dto/WBSDTO.java
@@ -1,4 +1,85 @@
 package com.structurax.root.structurax.root.dto;
 
 public class WBSDTO {
+    private int taskId;
+    private String projectId;
+    private Integer parentId;
+    private String name;
+    private String status;
+    private Boolean milestone;
+
+    // Default constructor
+    public WBSDTO() {
+    }
+
+    // Parameterized constructor
+    public WBSDTO(int taskId, String projectId, Integer parentId, String name, String status, Boolean milestone) {
+        this.taskId = taskId;
+        this.projectId = projectId;
+        this.parentId = parentId;
+        this.name = name;
+        this.status = status;
+        this.milestone = milestone;
+    }
+
+    // Getters and Setters
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Boolean getMilestone() {
+        return milestone;
+    }
+
+    public void setMilestone(Boolean milestone) {
+        this.milestone = milestone;
+    }
+
+    @Override
+    public String toString() {
+        return "WBSDTO{" +
+                "taskId=" + taskId +
+                ", projectId='" + projectId + '\'' +
+                ", parentId=" + parentId +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", milestone=" + milestone +
+                '}';
+    }
 }
