40926273384cdf15b669d3f2289440602ce8875a
Thagshan@01 <arulthags01@gmail.com>
2025-10-16 02:39:44 +0530

    Supplier Order Fetching Completed

--- src/main/java/com/structurax/root/structurax/root/dao/SupplierDAO.java
+++ src/main/java/com/structurax/root/structurax/root/dao/SupplierDAO.java
@@ -4,19 +4,24 @@ import java.util.List;
 import java.util.Optional;
 
 import com.structurax.root.structurax.root.dto.CatalogDTO;
+import com.structurax.root.structurax.root.dto.PurchaseOrderDTO;
 import com.structurax.root.structurax.root.dto.SupplierDTO;
 
 public interface SupplierDAO {
 
+    // Existing catalog methods
     CatalogDTO createCatalog(CatalogDTO catalogDTO);
-
     List<CatalogDTO> getAllCatalogs();
-
     void deleteCatalog(Integer itemId);
-
     CatalogDTO getCatalogById(Integer itemId);
 
+    // Existing supplier methods
     Optional<SupplierDTO> findByEmail(String email);
-    
     SupplierDTO getSupplierById(Integer supplierId);
+
+    // New purchase order methods
+    List<PurchaseOrderDTO> getAllOrders();
+    PurchaseOrderDTO getOrderById(Integer orderId);
+    List<PurchaseOrderDTO> getOrdersBySupplierId(Integer supplierId);
+    List<PurchaseOrderDTO> getOrdersByProjectId(String projectId);
 }
