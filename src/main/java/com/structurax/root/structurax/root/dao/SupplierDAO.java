622dc1cf6a378668afef8c8f93743aca4c2e1455
Thagshan@01 <arulthags01@gmail.com>
2025-10-16 13:57:07 +0530

    Supplier Order modified Dtos

--- src/main/java/com/structurax/root/structurax/root/dao/SupplierDAO.java
+++ src/main/java/com/structurax/root/structurax/root/dao/SupplierDAO.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import com.structurax.root.structurax.root.dto.CatalogDTO;
 import com.structurax.root.structurax.root.dto.PurchaseOrderDTO;
 import com.structurax.root.structurax.root.dto.SupplierDTO;
+import com.structurax.root.structurax.root.dto.ProjectDTO;
+import com.structurax.root.structurax.root.dto.OrderItemDTO;
+import java.math.BigDecimal;
 
 public interface SupplierDAO {
 
@@ -24,4 +27,10 @@ public interface SupplierDAO {
     PurchaseOrderDTO getOrderById(Integer orderId);
     List<PurchaseOrderDTO> getOrdersBySupplierId(Integer supplierId);
     List<PurchaseOrderDTO> getOrdersByProjectId(String projectId);
+
+    ProjectDTO getProjectById(String projectId);
+    PurchaseOrderDTO getOrderByProjectId(String projectId);
+    BigDecimal getQuotationAmountByResponseId(Integer responseId);
+    List<OrderItemDTO> getOrderItemsByOrderId(Integer orderId);
+
 }
